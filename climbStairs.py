bae3bdf007d5511f5d4a7f89accac3cff68ec178
tim02468 <tim2538785@gmail.com>
2019-04-30 23:25:06 +0800

    Update climbStairs.py

--- climbStairs.py
+++ climbStairs.py
@@ -1,5 +1,20 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
+        # dp solution
+        if n == 1:
+            return 1
+        
+        # initial dp array
+        dp = [0 for _ in range(n)]
+        
+        dp[0] = 1
+        dp[1] = 2
+        
+        for i in range(2, n):
+            dp[i] = dp[i - 1] + dp[i - 2]
+        return dp[-1]
+        
+        
         # dp by memoize method
         def climb(step, n):
             if step > n:
